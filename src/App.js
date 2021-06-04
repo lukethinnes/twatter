69c20b804e5f308cdb6d9afadcd89612d2dbbfc9
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-04 17:31:57 -0600

    Updated README.md and began on sidebar component.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css';
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    // BEM
+    <>
+    <div className="app">     
+      <Sidebar />
+      
+      {/* Feed */}
+      
+      {/* Widgets */}
+
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './styles/Sidebar.css'
+import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home';
+import NotificationsNone from '@material-ui/icons/NotificationsNone'
+import SearchIcon from '@material-ui/icons/Search';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import MailOutlineIcon from '@material-ui/icons/MailOutline'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import ListAltIcon from '@material-ui/icons/ListAlt'
+import PermIdentityIcon from '@material-ui/icons/PermIdentity'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <TwitterIcon />
+            <SidebarOption Icon={HomeIcon} text='Home' />
+            <SidebarOption Icon={SearchIcon} text='Explore' />
+            <SidebarOption Icon={NotificationsNone} text='Notifications' />
+            <SidebarOption Icon={MailOutlineIcon} text='Messages' />
+            <SidebarOption Icon={BookmarkBorderIcon} text='Saved' />
+            <SidebarOption Icon={ListAltIcon} text='Lists' />
+            <SidebarOption Icon={PermIdentityIcon} text='My Account' />
+            <SidebarOption Icon={MoreHorizIcon} text='More' />
+            
+
+            {/* Twat Button */}
+        </div>
+    )
+}
+
+export default Sidebar;
