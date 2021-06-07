cd4f5696d20bebb061d0f69ac3d923019fef53d0
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-06 22:20:37 -0600

    Stylized feed and sidebar.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <div className="app">     
       <Sidebar />
       
-      {/* Feed */}
+      <Feed />
       
       {/* Widgets */}
 
---
+++ src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/Feed.css'
+
+
+function Feed() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    )
+}
+
+export default Feed
