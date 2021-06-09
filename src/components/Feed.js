324ac2743538051115091bd4fc9b59a10525fc74
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-08 21:49:27 -0600

    Added widgets.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,8 +4,8 @@ import './styles/Feed.css'
 
 function Feed() {
     return (
-        <div>    
-        <div>
+        <div className='feed'>    
+        <div classname='feed__header'>
             <h2>Home</h2>
         </div>
         </div>
