71840bbdbbe51b11fddc7baa7cb7272a0ed622d5
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-20 21:44:51 -0600

    Connected to firebase.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TwatBox from './TwatBox.js';
 import './styles/Feed.css';
 import Post from './Post.js';
+import db from '../firebase.js'
 
 
 function Feed() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, [])
+
     return (
         <div className='feed'>    
         <div className='feed__header'>
