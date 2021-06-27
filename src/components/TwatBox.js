05ec45ba2d637793b607035cbdd69f0c8b605e57
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-26 18:43:58 -0600

    Imported state to message section.

--- src/components/TwatBox.js
+++ src/components/TwatBox.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './styles/TwatBox.css'
 import { Avatar, Button } from '@material-ui/core'
 
 function TwatBox() {
+    const[twat, setTwat] = useState('');
     return (
         <div className='twatBox'>
             <form>
                 <div className='twatBox__input'>
                     <Avatar src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FvEJns8TcNKA%2Fmaxresdefault.jpg&f=1&nofb=1'></Avatar>
-                <input placeholder="Let it out... Really let it out..." type='text'></input>
+                <input value={twat} type='text'></input>
                 {/* <input placeholder="Enter image URL..." type='text'></input> */}
                 </div>
                 <Button className='twatBox__twatButton'>Twat</Button>
