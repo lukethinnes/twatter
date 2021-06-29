6dd70b832eb587467cfece0302c0d34a509def3a
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-28 23:02:55 -0600

    Working on sending data to database.

--- src/components/TwatBox.js
+++ src/components/TwatBox.js
@@ -1,17 +1,40 @@
 import React, {useEffect, useState} from 'react'
 import './styles/TwatBox.css'
 import { Avatar, Button } from '@material-ui/core'
+import db from './firebase'
 
 function TwatBox() {
     const[twat, setTwat] = useState('');
+    const[image, setImage] = useState('');
+
+    const sendTwat = event => {
+        event.preventDefault();
+        db.collection('post').add({
+            displayName: 'Hard Thomas',
+            username: 'hardthomas',
+            verified: true,
+            text: twat,
+        })
+    }
+
     return (
         <div className='twatBox'>
             <form>
                 <div className='twatBox__input'>
                     <Avatar src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FvEJns8TcNKA%2Fmaxresdefault.jpg&f=1&nofb=1'></Avatar>
-                <input value={twat} type='text'></input>
-                {/* <input placeholder="Enter image URL..." type='text'></input> */}
+                <input 
+                    onChange={event => setTwat(event.target.value)} 
+                    value={twat} 
+                    type='text'
+                />
                 </div>
+                <input
+                    value={image}
+                    onChange={event => setImage(event.target.value)}
+                    className='twatBox__imageInput'
+                    placeholder='Optional: Enter image URL'
+                    type='text'
+                />
                 <Button className='twatBox__twatButton'>Twat</Button>
             </form>
         </div>
